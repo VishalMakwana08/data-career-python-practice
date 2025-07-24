6cadc1cadb445ca7332017fce92e178f418d6ab8
VishalMakwna08 <makwanavishal588@gmail.com>
2025-07-24 20:07:43 +0530

    python programs

---
+++ python/temperature-conversion.py
@@ -0,0 +1,21 @@
+# Convert temperature from Celsius to Fahrenheit and vice versa using formula.
+
+while(True):
+    print("1.Celsius to Fahrenheit")
+    print("2.Fahrenheit to Celsius")
+    print("3.Exit")
+    choice=int(input("Enter Any Option:"))
+    match choice:
+        case 1:
+            celsius=float(input("Enter Celsius:"))
+            fahrenhit=celsius*9/5+32
+            print(f"Fahrenheit:{fahrenhit}")
+        case 2:
+            fahrenhit=float(input("Enter Fahrenhit:"))
+            celsius=(fahrenhit-32)*(5/9)
+            print(f"Celsius:{celsius}")
+        case 3:
+            break
+        case _:
+            print("Invalid Option")
+            break
