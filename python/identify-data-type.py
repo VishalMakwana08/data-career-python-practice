dd0bfdefd966a60cf76731c0b9f1403991c974ce
VishalMakwna08 <makwanavishal588@gmail.com>
2025-07-24 19:53:07 +0530

    pyhon programs

---
+++ python/identify-data-type.py
@@ -0,0 +1,21 @@
+#Identify the Data Type
+
+#int
+value=10
+print(f"Value:{value} Type:{type(value)}")
+
+#float
+value=10.10
+print(f"Value:{value} Type:{type(value)}")
+
+#complex
+value=10+10j
+print(f"Value:{value} Type:{type(value)}")
+
+#bool
+value=True
+print(f"Value:{value} Type:{type(value)}")
+
+#str
+value="Hello"
+print(f"Value:{value} Type:{type(value)}")
