1391de6c9f7af07c2557fb6a27d0bb7c51792638
VishalMakwna08 <makwanavishal588@gmail.com>
2025-08-02 19:27:53 +0530

    pattern program saved

---
+++ python/star_patterns/hollow-triangle.py
@@ -0,0 +1,11 @@
+#hollow triangle 
+n=5
+for i in range(1,6):
+    n-=1
+    print(n*" ",end="")#for right side space
+    for j in range(1,i+1):
+        if j==i or j==1 or i==5: #for actual position *
+            print("* ",end="")
+        else:
+            print("  ",end="")#for space inside in a triang;e
+    print("")
