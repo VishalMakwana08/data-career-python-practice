b025af3f33aff084059dcbd60b6381cd7c6b987a
VishalMakwna08 <makwanavishal588@gmail.com>
2025-08-10 09:32:10 +0530

    program saved

---
+++ python/exercise/exercise6.py
@@ -0,0 +1,14 @@
+# Write Python programs to work with the following:  
+# i) input( )  
+# ii) print( )  
+# iii) ‘sep’ attribute  
+# iv) ‘end’ attribute  
+# v) replacement Operator ({ }) 
+
+a=int(input("Enter Any Number:"))#input() use
+
+print(a)#print() use
+
+print(f"Value Of A Is {a}",end="\t") #replacement Operator ({ }) and 'end' attribute
+
+print("hello","world",sep="--")#'sep' attribut use
