2e9c5551338a5b20c21a03bf811c82f5e35e73fa
VishalMakwna08 <makwanavishal588@gmail.com>
2025-08-10 09:03:08 +0530

    program saved

---
+++ python/exercise/exercise3.py
@@ -0,0 +1,17 @@
+# Write a Python program to work with following functions: 
+# id(), type(),range() 
+
+a=10
+
+#display value of a
+print(f"Value:{a}")
+
+#display type of a
+print(f"DataType:{type(a)}")
+
+#display id of a
+print(f"Id:{id(a)}")
+
+#use of range
+r=list(range(1,11))
+print(r)
