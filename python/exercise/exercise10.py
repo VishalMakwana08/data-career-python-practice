73f28e65da49989035ddfbf35e0cdc9fe6d0323f
VishalMakwna08 <makwanavishal588@gmail.com>
2025-08-10 09:53:54 +0530

    program saved

---
+++ python/exercise/exercise10.py
@@ -0,0 +1,12 @@
+#  Write a Python program to work with the various ways of accessing the string.  
+# i) By using Indexing (Both Positive and Negative) ii) By using Slice Operator 
+
+message="Hello Python"
+
+#print string using slice operator positive indexing
+
+print(message[::])
+
+#reverse string using slice operator negative indexing
+
+print(message[::-1])
