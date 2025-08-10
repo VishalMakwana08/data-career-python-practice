c6f4cc694292b3f115a110e8ea0a9e20521eabf2
VishalMakwna08 <makwanavishal588@gmail.com>
2025-08-10 18:45:33 +0530

    removed

--- python/exercise/exercise1.py
+++
@@ -1,7 +0,0 @@
-# Write a Python program to display as following:  
-# Hello World. 
-# This is Python. 
-
-print("Hello World")
-
-print("This is Python")
--- python/exercise/exercise12.py
+++
@@ -1,9 +0,0 @@
-#  Write a Python program to copy the contents of a file to another file.
-
-f=open("E:\\BCA-SEM-5\\CS-30\\data-career-python-practice\\python\\exercise\\hello.txt","r")
-
-f2=open("E:\\BCA-SEM-5\\CS-30\\data-career-python-practice\\python\\exercise\\hello_copy.txt","w")
-#create new file name with hello_copy.txt and copy content of the hello.txt file
-f2.write(f.read())
-f2.close()
-f.close()
